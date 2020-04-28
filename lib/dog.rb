c2b0ff54943337c9d1f86e2957b33bfd6d66fb95
Joseph Romero <joeromero47@gmail.com>
2020-04-28 18:34:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,12 @@ class Dog
   def name = (dogs_name)
     @this_dogs_name = dogs_name
   end
+  def bark
+    @woof = puts "woof!"
+  end
   def name
     @this_dogs_name
     @woof
-  end
-  def bark
-    @woof = puts "woof!"
   end
 end
   
